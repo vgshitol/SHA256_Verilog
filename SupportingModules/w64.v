5c583ac1ea83b1f92b5a2d9dde3986921b2ae324
vgshitol <vgshitol@ncsu.edu>
2018-11-01 00:22:04 -0400

    Added W Vector Not Working

---
+++ SupportingModules/w64.v
@@ -0,0 +1,39 @@
+module w64 #(parameter W_LENGTH = 64
+) (
+
+    /*-----------Inputs--------------------------------*/
+
+    input                                   clock,  /* clock */
+    input                                   reset,
+    input  reg                              enable, /* Previous Enable to decide what to do for the next enable*/
+    input  reg                              w_index_complete,
+    input  reg [ $clog2(W_LENGTH):0]        w_vector_index,
+    input  reg [511:0]                      message_vector,
+    input  reg [2095:0]                     prev_w_vector,
+    
+    /*-----------Outputs--------------------------------*/
+
+    output reg                              w_vector_complete,  /* message formation complete flag */
+    output reg [2095:0]                     w_vector
+);
+
+integer block_bit;
+
+always @(posedge clock)
+    begin
+        if(reset || !enable) begin
+            w_vector = 0;
+            w_vector_complete <= 0;
+        end
+        else begin
+	    if(w_vector_index == 0) w_vector <= 0;
+	    else w_vector <= prev_w_vector;
+            
+	    if(!w_index_complete && w_vector_index < 16) for (block_bit = 0 ; block_bit < 8; block_bit = block_bit + 1)
+                w_vector[block_bit + w_vector_index*8] <= message_vector[504 + block_bit - w_vector_index*8];
+            
+	    if(w_index_complete) w_vector_complete <= 1;		               
+        end
+    end
+
+endmodule
--- verilog/MyDesign.v
+++ verilog/MyDesign.v
@@ -66,14 +66,27 @@ module MyDesign #(parameter OUTPUT_LENGTH       = 8,
   //<<<<----  YOUR CODE HERE    ---->>>>
 	`include "../SupportingModules/counter.v"
 	`include "../SupportingModules/msgEn.sv"
-	`include "../SupportingModules/msg512Block.sv"	
+	`include "../SupportingModules/msg512Block.sv"
+	`include "../SupportingModules/w64.v"		
 	reg address_read_complete;
 	reg message_vector_complete;
+	reg w_vector_enable;
 	reg [511:0] message_vector;
+	parameter W_LENGTH = 64;
+	reg  [ $clog2(W_LENGTH)-1:0]   w_vector_index;  // index of w
+	reg w_vector_index_complete;
+	reg [2095:0] w_vector;
+	reg w_vector_complete;
 
+/** Creating the Initial 512 Bit Block **/
 	msgEn u0(.clock(clk), .reset(reset), .start(xxx__dut__go), .enable(dut__msg__enable));
 	counter #(.MAX_MESSAGE_LENGTH(MAX_MESSAGE_LENGTH)) u1(.clock(clk), .reset(reset), .start(dut__msg__enable), .msg_length(xxx__dut__msg_length), .read_address(dut__msg__address), .read_complete(address_read_complete));
 	msg512Block #(.MSG_LENGTH(MAX_MESSAGE_LENGTH)) u2(.clock(clk), .reset(reset), .enable(dut__msg__enable), .address_read_complete(address_read_complete), .msg_address(dut__msg__address), .msg_write(dut__msg__write), .msg_data(msg__dut__data) , .prev_message_vector(message_vector), .message_vector_complete(message_vector_complete), .message_vector(message_vector));
+
+/** Creating the W Vector**/
+	msgEn u3(.clock(clk), .reset(reset), .start(address_read_complete), .enable(w_vector_enable));
+	counter #(.MAX_MESSAGE_LENGTH(W_LENGTH)) u4(.clock(clk), .reset(reset), .start(w_vector_enable), .msg_length(W_LENGTH), .read_address(w_vector_index), .read_complete(w_vector_index_complete));
+	w64 #(.W_LENGTH(W_LENGTH)) u5(.clock(clk), .reset(reset), .enable(w_vector_enable), .w_vector_index(w_vector_index), .w_index_complete(w_vector_index_complete), .message_vector(message_vector), .prev_w_vector(w_vector), .w_vector_complete(w_vector_complete), .w_vector(w_vector));
  
 endmodule
 
