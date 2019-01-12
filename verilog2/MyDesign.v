834f2b63f5b947a6550921740b0978feee18b816
vgshitol <vgshitol@ncsu.edu>
2019-01-12 16:10:08 -0500

    Working
    hash generated

--- verilog2/MyDesign.v
+++ verilog2/MyDesign.v
@@ -230,10 +230,24 @@ module MyDesign #(parameter OUTPUT_LENGTH       = 8,
 
         //K vector
     reg  [ $clog2(NUMBER_OF_Ks)-1:0]         kmemAddress;  // address of letter
+    reg  [ $clog2(NUMBER_OF_Ks)-1:0]         kmemAddressPipe1;
+    reg  [ $clog2(NUMBER_OF_Ks)-1:0]         kmemAddressPipe2;
+
     reg  kmemAddressCarry;
     reg                                      kmemEnable;
+    reg                                      kmemEnablePipe1;
+    reg                                      kmemEnablePipe2;
     reg                                      kmemWrite;
     reg [31:0]                               kmemData;  // read each letter
+
+    reg hashUpdateEnable;
+
+    always @(posedge clk) begin
+        kmemEnablePipe1 <= kmemEnable;
+        kmemEnablePipe2 <= kmemEnablePipe1;
+        hashUpdateEnable <= kmemEnablePipe2;
+    end
+
 
     always @(posedge clk)
         begin
@@ -251,6 +265,21 @@ module MyDesign #(parameter OUTPUT_LENGTH       = 8,
             if(resetSHA) kmemEnable <= 0;
             else kmemEnable <= ((msgAddressCompletePipe2 && hmemAddressCompletePipe2) || kmemEnable) && !finish;
         end
+
+    reg kmemAddressComplete;
+    reg kmemAddressCompletePipe1;
+    reg kmemAddressCompletePipe2;
+
+    always @(posedge clk)
+        begin
+            kmemAddressPipe1 <= kmemAddress;
+            kmemAddressPipe2 <= kmemAddressPipe1;
+
+
+            kmemAddressCompletePipe1 <= kmemAddressComplete;
+            kmemAddressCompletePipe2 <= kmemAddressCompletePipe1;
+        end
+
 
     genvar msgIndex;
     generate
@@ -279,23 +308,73 @@ module MyDesign #(parameter OUTPUT_LENGTH       = 8,
             else startNewW <= startNewW;
         end
 
+
+    reg [1:0] hashCycle;
+    reg getNewHash;
+    reg [5:0] kmemAddressStall;
+
+    always @(posedge clk)
+        begin
+            if(getNewHash) kmemAddressStall <= kmemAddressPipe2;
+            else kmemAddressStall <= kmemAddressStall;
+        end
+
+
+    always @(posedge clk) begin
+        if(!kmemEnablePipe1) begin
+            hashCycle <= 3;
+        end
+        else begin
+            hashCycle <= hashCycle + 1;
+        end
+
+        getNewHash = (hashCycle == 3);
+    end
+
+    reg updateEnable;
+
     genvar nwIndex;
     generate
         for (nwIndex=0; nwIndex< 16; nwIndex=nwIndex+1) begin
             always @(posedge clk) begin
-                if(!startNewW) w_vector[511 - nwIndex*32 -: 32] <= msgVector[511 - nwIndex*32 -: 32];
-                else if(startNewW && (nwIndex == kmemAddress - 16) && (kmemAddress < 32) && (kmemAddress >= 16))w_vector[511 - nwIndex*32 -: 32] <= new_w;
-                else if(startNewW && (nwIndex == kmemAddress - 32) && (kmemAddress < 48) && (kmemAddress >= 32))w_vector[511 - nwIndex*32 -: 32] <= new_w;
-                else if(startNewW && (nwIndex == kmemAddress - 48) && (kmemAddress < 64) && (kmemAddress >= 48))w_vector[511 - nwIndex*32 -: 32] <= new_w;
+                if(!kmemEnable || resetSHA) w_vector[511 - nwIndex*32 -: 32] <= 0;
+                else if(!hashUpdateEnable && !kmemAddressCompletePipe2) w_vector[511 - nwIndex*32 -: 32] <= msgVector[511 - nwIndex*32 -: 32];
+                else if(hashUpdateEnable && !kmemAddressCompletePipe2 && getNewHash && (nwIndex == kmemAddressStall[3:0])) w_vector[511 - nwIndex*32 -: 32] <= new_w;
                 else w_vector[511 - nwIndex*32 -: 32] <= w_vector[511 - nwIndex*32 -: 32];
             end
         end
     endgenerate
 
+    reg [3:0] index2;
+    reg [3:0] index7;
+    reg [3:0] index15;
+    reg [3:0] index16;
+
+    reg index2Carry;
+    reg index7Carry;
+    reg index15Carry;
+    reg index16Carry;
+
+
+    always @(*) begin
+        if(!startNewW) begin
+            index2 = 0;
+            index7 = 0;
+            index15 = 0;
+            index16 = 0;
+        end
+        else begin
+            {index2Carry,index2} = kmemAddressStall[3:0] - 2;
+            {index7Carry,index7} = kmemAddressStall[3:0] - 7;
+            {index15Carry,index15} = kmemAddressStall[3:0] - 15;
+            {index16Carry,index16} = kmemAddressStall[3:0] - 16;
+        end
+    end
+
     reg [31:0] s0word;
 
     always @(*) begin
-        s0word = w_vector[(kmemAddress%16-15)*32 + 31 -: 32];
+        s0word = w_vector[511 - index15*32 -: 32];
     end
 
     reg [31:0] s0w_r1;
@@ -315,46 +394,42 @@ module MyDesign #(parameter OUTPUT_LENGTH       = 8,
         sigma0_s0word <= (s0w_r1 ^ s0w_r2) ^ s0w_r3;
     end
 
-    reg kmemAddressComplete;
+    reg [31:0] s1word;
 
-    reg  [ $clog2(NUMBER_OF_Ks)-1:0]         kmemAddressPipe1;
-    reg  [ $clog2(NUMBER_OF_Ks)-1:0]         kmemAddressPipe2;
-
-    reg kmemAddressCompletePipe1;
-    reg kmemAddressCompletePipe2;
-
-    always @(posedge clk)
-        begin
-            kmemAddressPipe1 <= kmemAddress;
-            kmemAddressPipe2 <= kmemAddressPipe1;
-
-            kmemAddressCompletePipe1 <= kmemAddressComplete;
-            kmemAddressCompletePipe2 <= kmemAddressCompletePipe1;
-        end
-
-    reg kmemEnablePipe1;
-    reg kmemEnablePipe2;
-    reg hashUpdateEnable;
-
-    always @(posedge clk) begin
-        kmemEnablePipe1 <= kmemEnable;
-        kmemEnablePipe2 <= kmemEnablePipe1;
-        hashUpdateEnable <= kmemEnablePipe2;
+    always @(*) begin
+        s1word = w_vector[511 - index2*32 -: 32];
     end
 
-    reg [1:0] hashCycle;
-    reg getNewHash;
+    reg [31:0] s1w_r1;
+    reg [31:0] s1w_r2;
+    reg [31:0] s1w_r3;
+
+    always @(*) begin
+        s1w_r1 ={s1word[16:0], s1word[31:17]};
+        s1w_r2 = {s1word[18:0], s1word[31:19]};
+        s1w_r3[21:0] = s1word[31:10];
+        s1w_r3[31:22] = 0;
+    end
+
+    reg [31:0] sigma1_s1word;
 
     always @(posedge clk) begin
-        if(!kmemEnablePipe1) begin
-            hashCycle <= 3;
-        end
-        else begin
-            hashCycle <= hashCycle + 1;
-        end
-
-        getNewHash = (hashCycle == 3);
+        sigma1_s1word <= s1w_r1 ^ s1w_r2 ^ s1w_r3;
     end
+
+    reg [31:0] word7;
+    reg [31:0] word16;
+
+    always @(posedge clk) begin
+        word16 <= w_vector[511 - index16*32 -: 32];
+        word7 <= w_vector[511 - index7*32 -: 32];
+    end
+
+    always @(posedge clk) begin
+        new_w <= (sigma0_s0word + sigma1_s1word) + (word16 + word7);
+    end
+
+
 
         // Counter for K Vector
     always @(posedge clk)
@@ -536,7 +611,7 @@ module MyDesign #(parameter OUTPUT_LENGTH       = 8,
     end
 
     always @(posedge clk) begin
-        if(hashCycle == 2) begin
+        if(hashCycle == 2 && !kmemAddressComplete) begin
             newHash[32*0 +: 32] <= (t1 + t2) + wk;
             newHash[32*1 +: 32] <= a;
             newHash[32*2 +: 32] <= b;
@@ -545,6 +620,16 @@ module MyDesign #(parameter OUTPUT_LENGTH       = 8,
             newHash[32*5 +: 32] <= e;
             newHash[32*6 +: 32] <= f;
             newHash[32*7 +: 32] <= g;
+        end
+        else if(hashCycle == 2 && kmemAddressComplete && !kmemAddressCompletePipe2) begin
+            newHash[32*0 +: 32] <= (t1 + t2) + wk + hashVector[32*0 +: 32];
+            newHash[32*1 +: 32] <= a + hashVector[32*1 +: 32];
+            newHash[32*2 +: 32] <= b + hashVector[32*2 +: 32];
+            newHash[32*3 +: 32] <= c + hashVector[32*3 +: 32];
+            newHash[32*4 +: 32] <= t1d + wk + hashVector[32*4 +: 32];
+            newHash[32*5 +: 32] <= e + hashVector[32*5 +: 32];
+            newHash[32*6 +: 32] <= f + hashVector[32*6 +: 32];
+            newHash[32*7 +: 32] <= g + hashVector[32*7 +: 32];
         end
         else begin
             newHash[32*0 +: 32] <= newHash[32*0 +: 32];
@@ -558,8 +643,71 @@ module MyDesign #(parameter OUTPUT_LENGTH       = 8,
         end
     end
 
+        //hash vector
+     reg  [ $clog2(OUTPUT_LENGTH)-1:0]    domAddress;
+     reg  [31:0]                          domData;  // write data
+     reg                                  domEnable;
+     reg                                  domWrite;
 
+    always @(posedge clk)
+        begin
+            //output
+            dut__dom__address <= domAddress;
+            dut__dom__enable <= domEnable;
+            dut__dom__write <= domWrite;
+            dut__dom__data <= domData;
+        end
 
+    reg domAddressComplete;
+    reg domAddressCarry;
+
+    always @(posedge clk)
+        begin
+            if(resetSHA) domEnable <= 0;
+            else domEnable <= (kmemAddressCompletePipe2 || domEnable) && !finish;
+        end
+
+        // Counter for Hash Vector
+    always @(posedge clk)
+        begin
+            if(resetSHA || !domEnable)  begin
+                domAddress <= 0;
+                domAddressCarry <= 0;
+                domWrite <= 1;
+            end
+            else if(!domAddressCarry) begin
+                {domAddressCarry,domAddress} <= domAddress+1;
+                domWrite <= 0;
+            end
+            else if(domAddressCarry) begin
+                domAddress <= domAddress;
+                domAddressCarry <= domAddressCarry;
+                domWrite <= 0;
+            end
+            else begin
+                domAddress <= domAddress;
+                domAddressCarry <= domAddressCarry;
+                domWrite <= 1;
+            end
+
+            domAddressComplete <= domEnable && !resetSHA ? domAddressCarry || domAddressComplete: 0;
+
+        end
+
+    genvar domIndex;
+    generate
+        for (domIndex=0; domIndex<NUMBER_OF_Hs; domIndex=domIndex+1) begin
+            always @(posedge clk) begin
+                if(resetSHA || !domEnable) domData <= 0;
+                else if (!domAddressCarry && domIndex == domAddress) domData <= newHash[32*domIndex +: 32];
+                else domData <= domData;
+            end
+        end
+    endgenerate
+
+    always @(posedge clk) begin
+        finish <= domAddressComplete;
+    end
 
 
 endmodule
